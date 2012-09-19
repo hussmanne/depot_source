5db6d27df0f56f12db17a666290243eaecbae956
Hussein Dungarwalla <hussman@gmail.com>
2012-09-18 19:22:47 -0700

    Chapter 15, no es translation error message

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,22 @@
 #---
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
-  resources :users
-
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
-
-  resources :products do
-    get :who_bought, on: :member
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
   end
-
+end
   # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -80,7 +75,6 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root to: 'store#index', as: 'store'
   # ...
 
   # See how all your routes lay out with "rake routes"
@@ -88,4 +82,3 @@ Depot::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
